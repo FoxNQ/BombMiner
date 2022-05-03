956bccc2bd886b4b80901797aacd74c66402365a
FoxNgN <caonguyenkaitou99@gmail.com>
2022-05-03 17:15:43 +0700

    Add cPoint Class

---
+++ BombMiner/cPoint.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef cPoint_h
+#define cPoint_h
+
+class Point
+{
+private:
+	int x, y;
+
+public:
+	Point() { x = y = 0; }
+	Point(int& X, int& Y) { x = X; y = Y; }
+
+	void Update(int& X, int& Y) { x = X; y = Y; }
+
+	int getX() { return x; }
+
+	int getY() { return y; }
+};
+#endif
